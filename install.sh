d51123f12c194939bf617793cf88aa40958194b8
Jason Calleiro <jjcall@Jasons-MacBook-Pro.local>
2015-08-16 18:16:47 -0400

    removed system font change

--- install.sh
+++ install.sh
@@ -1,13 +1,6 @@
 #!/bin/sh
 export HOMEBREW_CASK_OPTS="--appdir=/Applications"
 
-echo Install all AppStore Apps at first!
-# no solution to automate AppStore installs
-read -p "Press any key to continue... " -n1 -s
-echo  '\n'
-
-echo Install and Set San Francisco as System Font
-ruby -e "$(curl -fsSL https://raw.github.com/wellsriley/YosemiteSanFranciscoFont/master/install)"
 echo Install Homebrew, Postgres, wget and cask
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 brew update
@@ -52,13 +45,10 @@ brew cask install adobe-creative-cloud
 brew cask install appcleaner
 brew cask install dropbox
 brew cask install vlc
-brew cask install iterm2
 brew cask install java
 
 # Development
 echo Install Dev Apps
-brew cask install virtualbox
-brew cask install vagrant
 brew cask install sequel-pro
 
 # Google Slavery

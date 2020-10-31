09af6ed1732cd99994356cff143f94e6bdc2335a
Jason Calleiro <jasoncalleiro@jasons-mac-mini.lan>
2020-10-30 17:33:19 -0700

    Remove ZSH install script, Remove Neovim install script

--- scripts/osx.sh
+++ scripts/osx.sh
@@ -15,21 +15,6 @@ fi
 
 echo ""
 
-# -- rbenv ---------------------------------------------------------------------
-
-if exists "rbenv"; then
-  echo_item "rbenv is already installed" green
-else
-  if get_boolean_response "Do you want to install rbenv?"; then
-    brew install rbenv ruby-install
-    rbenv rehash
-  else
-    echo_item "Skipping rbenv install" red
-  fi
-fi
-
-echo ""
-
 # -- npm -----------------------------------------------------------------------
 
 if exists "npm"; then
@@ -43,33 +28,6 @@ else
 fi
 
 echo ""
-
-# -- zsh -----------------------------------------------------------------------
-
-if exists "zsh"; then
-  echo_item "zsh is already installed" green
-else
-  if get_boolean_response "Do you want to install zsh?"; then
-    brew install zsh
-  else
-    echo_item "Skipping zsh install" red
-  fi
-fi
-
-echo ""
-
-# -- Neovim --------------------------------------------------------------------
-
-if exists "nvim"; then
-  echo_item "Neovim is already installed" green
-else
-  if get_boolean_response "Do you want to install Neovim?"; then
-    brew install --HEAD neovim
-    curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-  else
-    echo_item "Skipping Neovim install" red
-  fi
-fi
 
 # -- Brew Apps----------------------------------------------------------------------
 if exists "brew"; then
